a47a9a440e2936e1b8d93b9f77b57d53571555b1
GABRIELI KUBIAKOSKI <108832324+gabrielekubiakoski@users.noreply.github.com>
2025-06-04 08:12:24 -0300

    2345

--- main.js
+++ main.js
@@ -16,19 +16,19 @@ const tempoObjetivo2 = new Date ("2030-01-01T00:00:00");
 const tempoObjetivo3 = new Date ("2050-01-01T00:00:00");
 const tempoObjetivo4 = new Date ("2026-01-01T00:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
-function atualizarCronometro (){
+function atualizaCronometro (){
     for (let i = 0; i < contadores.length; i++){
-        document.getElementById("dias"+i).textContent = calculatempo(tempos [i])[0];
-        document.getElementById("horas"+i).textContent = calculatempo(tempos [i])[1];
-        document.getElementById("min"+i).textContent = calculatempo(tempos [i])[2];
-        document.getElementById("seg"+i).textContent = calculatempo(tempos [i])[3];
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos [i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos [i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos [i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos [i])[3];
     }
     for (let i = 0; i < contadores.length; i++){
         //contadores[i].textContent = calculaTempo (tempos [i]);
     }
 }
 function comecaCronometro (){
-    atualizarCronometro ();
+    atualizaCronometro ();
     setInterval (atualizarCronometro, 1000);
 }
 comecaCronometro();
